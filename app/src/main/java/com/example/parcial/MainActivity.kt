bc0f1eeabf13a5af8fb136007c0568be248e3876
Juan Fox <juannfox@hotmail.com>
2024-05-31 07:46:00 -0300

    Conectar navgraph con bottom bar

--- app/src/main/java/com/example/parcial/MainActivity.kt
+++ app/src/main/java/com/example/parcial/MainActivity.kt
@@ -11,12 +11,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.parcial.ui.theme.ParcialTheme
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var bottomNavView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // TODO
+
+        // Fragment container con navgraph
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fc_main) as NavHostFragment
+        // Barra/menu inferior
+        bottomNavView = findViewById(R.id.bottom_bar)
+        // Conectar navgraph con menu inferior
+        NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
     }
 }
