6e0e62978b902f2e61555c8afe0d0979d34ec906
Juan Fox <juannfox@hotmail.com>
2024-06-01 19:41:22 -0300

    restaurar main act

--- app/src/main/java/com/example/parcial/activities/MainActivity.kt
+++ app/src/main/java/com/example/parcial/activities/MainActivity.kt
@@ -11,18 +11,15 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var bottomNavView: BottomNavigationView
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(R.layout.activity_main)
 
         // Fragment container con navgraph
         navHostFragment = supportFragmentManager.findFragmentById(R.id.fc_main) as NavHostFragment
         // Barra/menu inferior
-        bottomNavView = binding.bottomBar
+        bottomNavView = findViewById(R.id.bottom_bar)
         // Conectar navgraph con menu inferior
         NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
 
