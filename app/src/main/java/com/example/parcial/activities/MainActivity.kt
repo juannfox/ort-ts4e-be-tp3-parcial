831d3970da7309e4b7141a54072a07ada634e88d
Juan Fox <juannfox@hotmail.com>
2024-06-01 19:31:41 -0300

    View binding en main

--- app/src/main/java/com/example/parcial/activities/MainActivity.kt
+++ app/src/main/java/com/example/parcial/activities/MainActivity.kt
@@ -5,20 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.parcial.R
+import com.example.parcial.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var bottomNavView: BottomNavigationView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_main)
 
         // Fragment container con navgraph
         navHostFragment = supportFragmentManager.findFragmentById(R.id.fc_main) as NavHostFragment
         // Barra/menu inferior
-        bottomNavView = findViewById(R.id.bottom_bar)
+        bottomNavView = binding.bottomBar
         // Conectar navgraph con menu inferior
         NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
 
