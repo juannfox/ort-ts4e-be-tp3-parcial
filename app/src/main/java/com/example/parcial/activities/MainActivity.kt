3ea8ba8f67c260d9310a78575c9df2443c84ff1c
Nicokevo <82186948+Nicokevo@users.noreply.github.com>
2024-06-02 21:12:44 -0300

    Drawer menu and layout

--- app/src/main/java/com/example/parcial/activities/MainActivity.kt
+++ app/src/main/java/com/example/parcial/activities/MainActivity.kt
@@ -1,15 +1,20 @@
+package com.example.parcial.activities
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.parcial.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navHostFragment: NavHostFragment
+    private lateinit var bottomNavView: BottomNavigationView
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
 
@@ -17,29 +22,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Configuración de la Toolbar como ActionBar
+        // Configurar Toolbar
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        // Referencias al DrawerLayout y NavigationView
         drawerLayout = findViewById(R.id.drawer_layout)
         navigationView = findViewById(R.id.nav_view)
-
-        // Fragment container con NavHostFragment
         navHostFragment = supportFragmentManager.findFragmentById(R.id.fc_main) as NavHostFragment
+        bottomNavView = findViewById(R.id.bottom_bar)
 
-        // Configuración del AppBarConfiguration
-        val appBarConfiguration = AppBarConfiguration.Builder(navHostFragment.navController.graph)
-            .setOpenableLayout(drawerLayout)
-            .build()
+        NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
 
-        // Conectar el NavController con la ActionBar y el NavigationView
-        NavigationUI.setupActionBarWithNavController(this, navHostFragment.navController, appBarConfiguration)
-        NavigationUI.setupWithNavController(navigationView, navHostFragment.navController)
+        setupDrawerLayout()
+    }
+
+    private fun setupDrawerLayout() {
+        val navController = navHostFragment.navController
+        navigationView.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = navHostFragment.navController
         return NavigationUI.navigateUp(navController, drawerLayout) || super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
