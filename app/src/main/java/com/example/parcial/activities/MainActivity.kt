8f0652abfe7df466a6bbd5cec76c38df6259ffa5
Nicokevo <82186948+Nicokevo@users.noreply.github.com>
2024-06-03 23:57:10 -0300

    Drawer Menu

--- app/src/main/java/com/example/parcial/activities/MainActivity.kt
+++ app/src/main/java/com/example/parcial/activities/MainActivity.kt
@@ -1,26 +1,55 @@
-package com.example.parcial.activities
-
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.parcial.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var bottomNavView: BottomNavigationView
 
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var drawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Fragment container con navgraph
+        // Obtener el fragmento de navegación y la vista de navegación
         navHostFragment = supportFragmentManager.findFragmentById(R.id.fc_main) as NavHostFragment
-        // Barra/menu inferior
+
+        // Configurar la barra de navegación inferior
         bottomNavView = findViewById(R.id.bottom_bar)
-        // Conectar navgraph con menu inferior
         NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
 
+        // Configurar el DrawerLayout y el ActionBarDrawerToggle
+        drawerLayout = findViewById(R.id.drawer_layout)
+        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(drawerToggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+        // Sincronizar el estado del ActionBarDrawerToggle con el DrawerLayout
+        drawerToggle.syncState()
+
+        // Obtener el NavigationView y vincularlo con el NavController
+        navigationView = findViewById(R.id.nav_view)
+        NavigationUI.setupWithNavController(navigationView, navHostFragment.navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Manejar los clics del botón de hamburguesa
+        return if (drawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
