8289083977bc7b63b9b6e0d091936c574556b526
Diego Sperber <diegosperber@Diegos-MBP.home>
2024-06-06 01:41:13 +0200

    search-view2 updated - missing action bar

--- app/src/main/java/com/example/parcial/fragments/RoundTripFragment.kt
+++ app/src/main/java/com/example/parcial/fragments/RoundTripFragment.kt
@@ -1,17 +1,41 @@
 package com.example.parcial.fragments
 
+import android.app.DatePickerDialog
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.parcial.R
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import java.util.Calendar
+
 
 class RoundTripFragment : Fragment() {
+
+    lateinit var btnNewDestination : Button
+    lateinit var viewRoundTrip: View
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_round_trip, container, false)
+        viewRoundTrip = inflater.inflate(R.layout.fragment_round_trip, container, false)
+        btnNewDestination = viewRoundTrip.findViewById(R.id.btn_new_destination)
+
+        btnNewDestination.setOnClickListener {
+            Snackbar.make(viewRoundTrip, R.string.new_route, Snackbar.LENGTH_SHORT)
+                .show()
+        }
+
+
+
+        return viewRoundTrip
     }
+
+
+
 }
